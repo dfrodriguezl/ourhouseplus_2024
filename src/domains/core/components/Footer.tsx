e5ce3971a1945dc5ffbd199e52257acc1b4b0623
Diego Fernando Rodriguez Lamus <dfrodriguezl@dane.gov.co>
2021-07-31 23:01:33 -0500

    New footer

--- src/domains/core/components/Footer.tsx
+++ src/domains/core/components/Footer.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { compose } from 'recompose';
-import { Box, Container, createStyles, Grid, makeStyles, Theme } from '@material-ui/core';
+import { Box, Container, createStyles, Grid, makeStyles, Theme, Typography } from '@material-ui/core';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { Facebook, Instagram, LinkedIn } from '@material-ui/icons';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 
 import { logoSmallWhite } from 'assets'
 
@@ -10,8 +12,8 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: 'flex',
-      flexDirection: 'column',
-      backgroundColor: '#707070',
+      flexDirection: 'row',
+      backgroundColor: '#010100',
       padding: '20px'
     },
     footer: {
@@ -19,7 +21,8 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: theme.spacing(2, 2),
       lineHeight: '16px',
       '& a': {
-        color: theme.palette.common.white,
+        // color: theme.palette.common.white,
+        color: 'white',
         opacity: '89%'
       }
     },
@@ -28,71 +31,103 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     bottomLinks: {
       float: 'right',
+      marginRight: 20,
       paddingTop: 5,
       '& img': {
         paddingLeft: 10
       },
       [theme.breakpoints.down('sm')]: {
-        float: 'left',
+        textAlign: 'center',
+        marginRight: 0,
+        float: 'none'
       }
     },
     socialButtons:{
       [theme.breakpoints.down('sm')]: {
-        marginTop: 15
+        marginTop: 15,
+        textAlign: 'center',
+      }
+    },
+    item: {
+      marginRight: 70,
+      [theme.breakpoints.down('sm')]: {
+        marginRight: 0,
+        display: 'block',
+        textAlign: 'center',
       }
     }
   })
 );
 
+interface propsClasses {
+  classes: any;
+}
+
 type Props = RouteComponentProps;
 const Footer = (props: Props) => {
   const classes = useStyles()
+  const theme = useTheme();
+  const smallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <Grid container item className={classes.root} xs={12} justify="flex-end">
+    <Grid container item className={classes.root} xs={12}>
       <Container maxWidth="lg">
         <footer className={classes.footer}>
           <Grid container>
-            <Grid item xs={12} sm={2}>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/about">About</Link></Box>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/how-it-works">How rea works</Link></Box>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/news">News</Link></Box>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/leadership">Leadership</Link> </Box>  
-              <Box fontSize={14} lineHeight={1.7}><Link to="/">Job opportunities</Link> </Box>
+            <Grid item xs={12} sm={6}>
+              <Box component="span" fontSize={14} lineHeight={1.7} className={classes.item}><Link to="/about">About</Link></Box>
+              <Box component="span" fontSize={14} lineHeight={1.7} className={classes.item}><Link to="/how-it-works">How it works</Link></Box>
+              <Box component="span" fontSize={14} lineHeight={1.7} className={classes.item}><Link to="/news">News</Link></Box>
+              <Box component="span" fontSize={14} lineHeight={1.7} className={classes.item}><Link to="/contact-us">Contact</Link></Box>
             </Grid>
-            <Grid item xs={12} sm={2}>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/">Investors</Link></Box>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/contact-us">Contact rea</Link></Box>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/">Community</Link> </Box>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/">Share rea</Link></Box>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/">Support</Link></Box>
-              <Box fontSize={14} lineHeight={1.7}><Link to="/">Help center</Link></Box>
-            </Grid>
-            <Grid item xs={6} />
-            <Grid item xs={12} sm={1} className={classes.socialButtons}>
-              <a href="https://www.linkedin.com/company/rea-web/" className={classes.links}>
-                  <LinkedIn />
-              </a>
-              <a href="https://www.linkedin.com/company/rea-web/" className={classes.links}>
-                  <Facebook />
-              </a>
-              <a href="https://www.linkedin.com/company/rea-web/">
-                  <Instagram />
-              </a> 
-            </Grid>
+            {smallScreen?
+              <Fragment>
+                <SocialNetwork classes={classes}/>
+                <Grid item xs={12} sm={5}>
+                  <div className={classes.bottomLinks}>
+                    <Box fontSize={10} color='white'>
+                      Copyright © 2021 rea. All rights reserved
+                    </Box>
+                  </div>
+                </Grid>
+              </Fragment>:
+              <Fragment>
+                <Grid item xs={12} sm={5}>
+                  <div className={classes.bottomLinks}>
+                    <Box fontSize={10} color='white'>
+                      Copyright © 2021 rea. All rights reserved
+                    </Box>
+                  </div>
+                </Grid>
+                <SocialNetwork classes={classes}/>
+              </Fragment>
+          }
           </Grid>
-          <div className={classes.bottomLinks}>
-            <Box fontSize={10} color='white'>
-              Copyright © 2021 rea. All rights reserved
-              <Link to="/home">
-                <img src={logoSmallWhite} width={70} alt="logo" />
-              </Link>
-            </Box>
-          </div>
+          
         </footer>
       </Container>
     </Grid>
   );
+}
+
+const SocialNetwork = (propsClasses:propsClasses) => {
+
+  const {classes} = propsClasses;
+
+  return (
+    <Grid item xs={12} sm={1} className={classes.socialButtons} style={{ alignSelf: 'flex-end' }}>
+      <a href="https://www.linkedin.com/company/rea-web/" className={classes.links}>
+            <LinkedIn />
+        </a>
+        <a href="https://www.linkedin.com/company/rea-web/" className={classes.links}>
+            <Facebook />
+        </a>
+        <a href="https://www.linkedin.com/company/rea-web/">
+            <Instagram />
+        </a> 
+      </Grid>
+  )
+  
 }
 
 const container = compose<Props, {}>(
