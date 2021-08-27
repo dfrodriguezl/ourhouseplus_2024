fe5de4b1635e4cb6dd6c0544c20d2926e723c444
Alex Garcia <alexd.garcia@milwaukeetool.com>
2021-08-27 16:55:18 -0500

    Use hashistory

--- src/app/Auth0.tsx
+++ src/app/Auth0.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import history from 'app/history';
-import { Auth0Provider, AppState } from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 interface OwnProps {
   children: any;
@@ -11,16 +10,11 @@ const Auth0 = (props: Props) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN!;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID!;
 
-  const onRedirectCallback = (appState: AppState) => {
-    history.push(appState?.returnTo || window.location.pathname);
-  };
-
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin + '/home'}
-      onRedirectCallback={onRedirectCallback}
+      redirectUri={window.location.origin + '/#/home'}
     >
       {props.children}
     </Auth0Provider>
