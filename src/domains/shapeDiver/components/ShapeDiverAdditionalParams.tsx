64fc8ef30dc1126d930c3a9b876bd7961da34c2a
Diego <dfrodriguezl94@gmail.com>
2021-10-09 15:37:57 -0500

    Added decimal scale two numbers in additonal parameters

--- src/domains/shapeDiver/components/ShapeDiverAdditionalParams.tsx
+++ src/domains/shapeDiver/components/ShapeDiverAdditionalParams.tsx
@@ -1,6 +1,7 @@
 import { Accordion, AccordionDetails, AccordionSummary, Box, Grid, makeStyles } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { RootState } from 'app/store';
+import NumberFormat from 'react-number-format';
 import { connect } from 'react-redux';
 import { ModelData } from '../models';
 
@@ -58,52 +59,234 @@ const ShapeDiverAdditionalParams = (props: Props) => {
                 <Box fontSize={12}>Four bedroom</Box>
               </Grid>
               <Grid item xs={6} sm={4}>
-                <Box fontSize={12}>{modelData.averageBedroomPerDwelling}</Box>
-                <Box fontSize={12}>{modelData.greenSpacePerInhabitant}</Box>
+                <Box fontSize={12}>
+                  <NumberFormat
+                    value={modelData.averageBedroomPerDwelling}
+                    displayType="text"
+                    thousandSeparator
+                    decimalScale={2}
+                  />
+                </Box>
+                <Box fontSize={12}>
+                  <NumberFormat
+                    value={modelData.greenSpacePerInhabitant}
+                    displayType="text"
+                    thousandSeparator
+                    decimalScale={2}
+                  />
+                </Box>
                 <br />
-                <Box fontSize={12}>{modelData.groundFloorFreeSpace}</Box>
-                <Box fontSize={12}>{modelData.multipleFloorFreeSpace}</Box>
+                <Box fontSize={12}>
+                  <NumberFormat
+                    value={modelData.groundFloorFreeSpace}
+                    displayType="text"
+                    thousandSeparator
+                    decimalScale={2}
+                  />
+                </Box>
+                <Box fontSize={12}>
+                  <NumberFormat
+                    value={modelData.multipleFloorFreeSpace}
+                    displayType="text"
+                    thousandSeparator
+                    decimalScale={2}
+                  />
+                </Box>
                 <br />
                 <Grid xs={12} item container>
                   <Grid xs={4}>
                     <Box fontSize={12}>m2</Box>
-                    <Box fontSize={12}>{modelData.greenSpaceDensitym2}</Box>
-                    <Box fontSize={12}>{modelData.roadDensitym2}</Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.greenSpaceDensitym2}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.roadDensitym2}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
                   </Grid>
                   <Grid xs={4}>
                     <Box fontSize={12}>%</Box>
-                    <Box fontSize={12}>{modelData.greenSpaceDensity}</Box>
-                    <Box fontSize={12}>{modelData.roadDensity}</Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.greenSpaceDensity}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.roadDensity}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
                   </Grid>
                 </Grid>
                 <br />
                 <Grid xs={12} item container>
                   <Grid xs={4}>
                     <Box fontSize={12}>m2</Box>
-                    <Box fontSize={12}>{modelData.studiosm2}</Box>
-                    <Box fontSize={12}>{modelData.largeStudiosm2}</Box>
-                    <Box fontSize={12}>{modelData.oneBedroomm2}</Box>
-                    <Box fontSize={12}>{modelData.twoBedroomm2}</Box>
-                    <Box fontSize={12}>{modelData.threeBedroomm2}</Box>
-                    <Box fontSize={12}>{modelData.fourBedroomm2}</Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.studiosm2}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.largeStudiosm2}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.oneBedroomm2}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.twoBedroomm2}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.threeBedroomm2}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.fourBedroomm2}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
                   </Grid>
                   <Grid xs={4}>
                     <Box fontSize={12}>%</Box>
-                    <Box fontSize={12}>{modelData.studiosPorc}</Box>
-                    <Box fontSize={12}>{modelData.largeStudiosPorc}</Box>
-                    <Box fontSize={12}>{modelData.oneBedroomPorc}</Box>
-                    <Box fontSize={12}>{modelData.twoBedroomPorc}</Box>
-                    <Box fontSize={12}>{modelData.threeBedroomPorc}</Box>
-                    <Box fontSize={12}>{modelData.fourBedroomPorc}</Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.studiosPorc}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.largeStudiosPorc}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.oneBedroomPorc}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.twoBedroomPorc}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.threeBedroomPorc}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.fourBedroomPorc}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
                   </Grid>
                   <Grid xs={4}>
                     <Box fontSize={12}>nbr</Box>
-                    <Box fontSize={12}>{modelData.studios}</Box>
-                    <Box fontSize={12}>{modelData.largeStudios}</Box>
-                    <Box fontSize={12}>{modelData.oneBedroom}</Box>
-                    <Box fontSize={12}>{modelData.twoBedroom}</Box>
-                    <Box fontSize={12}>{modelData.threeBedroom}</Box>
-                    <Box fontSize={12}>{modelData.fourBedroom}</Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.studios}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.largeStudios}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.oneBedroom}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.twoBedroom}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.threeBedroom}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
+                    <Box fontSize={12}>
+                      <NumberFormat
+                        value={modelData.fourBedroom}
+                        displayType="text"
+                        thousandSeparator
+                        decimalScale={2}
+                      />
+                    </Box>
                   </Grid>
                 </Grid>
 
