7b06a4f50115db450aa6622b67276d9df6a8ad77
Diego <dfrodriguezl94@gmail.com>
2021-10-21 20:46:33 -0500

    Added upload terrain shape button in projects list

--- src/domains/core/containers/ListProjects.tsx
+++ src/domains/core/containers/ListProjects.tsx
@@ -13,6 +13,7 @@ import { connect } from 'react-redux';
 import { compose } from 'recompose';
 import { TopPanel } from 'domains/core/components';
 import { useAuth0 } from '@auth0/auth0-react';
+import { CloudUpload } from '@material-ui/icons';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -57,7 +58,7 @@ const useStyles = makeStyles(() =>
     },
     backgroundNew: {
       background: '#FFFFFF33 0% 0% no-repeat padding-box',
-      height: '30vh',
+      height: '15vh',
       width: '100%',
       marginTop: 10,
       display: 'flex'
@@ -66,10 +67,20 @@ const useStyles = makeStyles(() =>
       background: '#FFFFFF 0% 0% no-repeat padding-box',
       opacity: 0.9,
       border: '1px solid #707070',
-      marginBottom: 10,
-      height: '10vh',
-      width: '10vh',
+      marginBottom: 2,
+      height: '6vh',
+      width: '6vh',
       display: 'flex'
+    },
+    AddBoxRound: {
+      background: '#FFFFFF 0% 0% no-repeat padding-box',
+      opacity: 0.9,
+      border: '1px solid #707070',
+      marginTop: 6,
+      height: '6vh',
+      width: '15vh',
+      display: 'flex',
+      borderRadius: 45
     },
     text: {
       color: 'white',
@@ -118,7 +129,7 @@ interface DispatchProps {
 type Props = RouteComponentProps & StateProps & DispatchProps;
 export const ListProjects = (props: Props) => {
   const { loadProjectsByUsername, deleteProjectById, history, projects, loading } = props;
-  const { user } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   const classes = useStyles();
   const [hover, setHover] = useState(0);
 
@@ -130,137 +141,161 @@ export const ListProjects = (props: Props) => {
     history.push("/details/" + id)
   }
 
-  useEffect(() => {
-    if (user?.email) {
-      loadProjectsByUsername(user.email);
-    }
-  }, [loadProjectsByUsername, user])
-
-  return (
-    <Fragment>
-      {
-        loading &&
-        <FullPageOverlay />
+  const goToUploadShape = () => {
+    if (isAuthenticated && user) {
+      if (user['https://www.rea-web.com/roles'].includes('Administrator')) {
+        history.push("/uploadShape")
+      } else {
+        history.push("/home")
       }
-      <PageContainer background="black-model">
-        <Grid container xs={12} className={classes.topPanel} >
-          <TopPanel />
-          <Grid item xs={12}>
-            <Typography className={classes.textProfile}>
-              Finish setting up your profile to create sharable links and pdf documents
-            </Typography>
-          </Grid>
-          <Grid item container xs={3} direction="column">
-            <Button
-              className={classes.becomeMember}
-              startIcon={<AddIcon />}>
-              Finish your profile
-            </Button>
-            <Button className={classes.compareButton}>
-              Compare your projects
-            </Button>
-            <Typography variant="h6" className={classes.subtitleProjects}>
-              Your projects
-            </Typography>
-          </Grid>
-          <Grid item container xs={12}>
-            <Grid item container xs={2} className={classes.backgroundNew} direction="column" justify="center" alignItems="center">
-              <Box component="div" className={classes.AddBox} alignItems="center" justifyContent="center">
-                <IconButton onClick={() => goToHome()}>
-                  <AddSharpIcon style={{ fontSize: 80 }} />
-                </IconButton>
-              </Box>
-              <Typography variant="body2" className={classes.text}>
-                Create new
+    }
+  }
+
+    useEffect(() => {
+      if (user?.email) {
+        loadProjectsByUsername(user.email);
+      }
+    }, [loadProjectsByUsername, user])
+
+    return (
+      <Fragment>
+        {
+          loading &&
+          <FullPageOverlay />
+        }
+        <PageContainer background="black-model">
+          <Grid container xs={12} className={classes.topPanel} >
+            <TopPanel />
+            <Grid item xs={12}>
+              <Typography className={classes.textProfile}>
+                Finish setting up your profile to create sharable links and pdf documents
               </Typography>
             </Grid>
-            <Grid item xs={1}></Grid>
-            {projects.map((p, i) => {
-              const locationSaved: any = p?.location;
-              const densityGeneral = p?.location?.densityGeneral! ? p?.location?.densityGeneral! : p?.location?.density!;
-              const densityLocal = densityGeneral === 0 ? "suburban" : "urban";
+            <Grid item container xs={3} direction="column">
+              <Button
+                className={classes.becomeMember}
+                startIcon={<AddIcon />}>
+                Finish your profile
+              </Button>
+              <Button className={classes.compareButton}>
+                Compare your projects
+              </Button>
+              <Typography variant="h6" className={classes.subtitleProjects}>
+                Your projects
+              </Typography>
+            </Grid>
+            <Grid item container xs={12}>
+              <Grid item container xs={2} justify="center" alignItems="center">
+                <Grid item container xs={12} className={classes.backgroundNew} direction="column" justify="center" alignItems="center">
+                  <Box component="div" className={classes.AddBox} alignItems="center" justifyContent="center">
+                    <IconButton onClick={() => goToHome()}>
+                      <AddSharpIcon style={{ fontSize: 40 }} />
+                    </IconButton>
+                  </Box>
+                  <Typography variant="body2" className={classes.text}>
+                    Create new
+                  </Typography>
+                </Grid>
+                <Grid item container xs={12} className={classes.backgroundNew} direction="column" justify="center" alignItems="center">
+                  <Box component="div" className={classes.AddBoxRound} alignItems="center" justifyContent="center">
+                    <IconButton onClick={() => goToUploadShape()}>
+                      <CloudUpload style={{ fontSize: 40 }} />
+                    </IconButton>
+                  </Box>
+                  <Typography variant="body2" className={classes.text}>
+                    Upload your terrain shape
+                  </Typography>
+                  <br />
+                </Grid>
+              </Grid>
 
-              return (
-                <Fragment key={i}>
-                  <Grid item container xs={2}>
-                    <Grid item container className={classes.backgroundProject} direction="column" justify="center" alignItems="center">
-                      <Box component="div" alignItems="center" justifyContent="center">
-                        <IconButton onClick={() => goToProject(String(p.id))}>
-                          {
-                            locationSaved.densityGeneral === 0 ?
-                            <img alt={p.name} src={suburban} style={{ width: '90%', borderRadius: '50%' }} /> 
-                            :locationSaved[densityLocal] ?
-                              locationSaved[densityLocal].maxPriFloors <= 6 ?
-                                <img alt={p.name} src={height_6} style={{ width: '90%', borderRadius: '50%' }} /> :
-                                locationSaved[densityLocal].maxPriFloors <= 12 ?
-                                  <img alt={p.name} src={height_12} style={{ width: '90%', borderRadius: '50%' }} /> :
-                                  <img alt={p.name} src={height_13} style={{ width: '90%', borderRadius: '50%' }} /> :
-                              p?.location.maxPriFloors <= 6 ?
-                                <img alt={p.name} src={height_6} style={{ width: '90%', borderRadius: '50%' }} /> :
-                                p?.location.maxPriFloors <= 12 ?
-                                  <img alt={p.name} src={height_12} style={{ width: '90%', borderRadius: '50%' }} /> :
-                                  <img alt={p.name} src={height_13} style={{ width: '90%', borderRadius: '50%' }} />
-                          }
+              <Grid item xs={1}></Grid>
+              {projects.map((p, i) => {
+                const locationSaved: any = p?.location;
+                const densityGeneral = p?.location?.densityGeneral! ? p?.location?.densityGeneral! : p?.location?.density!;
+                const densityLocal = densityGeneral === 0 ? "suburban" : "urban";
 
-                        </IconButton>
-                      </Box>
-                      <Typography variant="body2" className={classes.text}>
-                        {p.projectName}
-                      </Typography>
-                    </Grid>
-                    <Grid item className={classes.containerOptions}>
-                      <Link href="#">
-                        <Typography className={classes.optionsProject}>
-                          Edit
-                          <EditIcon className={classes.optionsIcon} />
+                return (
+                  <Fragment key={i}>
+                    <Grid item container xs={2}>
+                      <Grid item container className={classes.backgroundProject} direction="column" justify="center" alignItems="center">
+                        <Box component="div" alignItems="center" justifyContent="center">
+                          <IconButton onClick={() => goToProject(String(p.id))}>
+                            {
+                              locationSaved.densityGeneral === 0 ?
+                                <img alt={p.name} src={suburban} style={{ width: '90%', borderRadius: '50%' }} />
+                                : locationSaved[densityLocal] ?
+                                  locationSaved[densityLocal].maxPriFloors <= 6 ?
+                                    <img alt={p.name} src={height_6} style={{ width: '90%', borderRadius: '50%' }} /> :
+                                    locationSaved[densityLocal].maxPriFloors <= 12 ?
+                                      <img alt={p.name} src={height_12} style={{ width: '90%', borderRadius: '50%' }} /> :
+                                      <img alt={p.name} src={height_13} style={{ width: '90%', borderRadius: '50%' }} /> :
+                                  p?.location.maxPriFloors <= 6 ?
+                                    <img alt={p.name} src={height_6} style={{ width: '90%', borderRadius: '50%' }} /> :
+                                    p?.location.maxPriFloors <= 12 ?
+                                      <img alt={p.name} src={height_12} style={{ width: '90%', borderRadius: '50%' }} /> :
+                                      <img alt={p.name} src={height_13} style={{ width: '90%', borderRadius: '50%' }} />
+                            }
+
+                          </IconButton>
+                        </Box>
+                        <Typography variant="body2" className={classes.text}>
+                          {p.projectName}
                         </Typography>
+                      </Grid>
+                      <Grid item className={classes.containerOptions}>
+                        <Link href="#">
+                          <Typography className={classes.optionsProject}>
+                            Edit
+                            <EditIcon className={classes.optionsIcon} />
+                          </Typography>
 
-                      </Link>
-                      <Link href="#" onMouseEnter={() => setHover(p.id)} onMouseLeave={() => setHover(0)}>
-                        <Typography className={classes.optionsProject}>
-                          {
-                            hover === p.id ?
-                              "Create and download pdf" :
-                              "Download pdf"
-                          }
-                          <img alt="download-pdf" src={download_white} className={classes.imgIcon} />
-                        </Typography>
-                      </Link>
-                      <div className={classes.optionsProject} onClick={() => deleteProjectById(p.id, user.email)}>
-                        Delete
-                        <DeleteIcon className={classes.optionsIcon} />
-                      </div>
+                        </Link>
+                        <Link href="#" onMouseEnter={() => setHover(p.id)} onMouseLeave={() => setHover(0)}>
+                          <Typography className={classes.optionsProject}>
+                            {
+                              hover === p.id ?
+                                "Create and download pdf" :
+                                "Download pdf"
+                            }
+                            <img alt="download-pdf" src={download_white} className={classes.imgIcon} />
+                          </Typography>
+                        </Link>
+                        <div className={classes.optionsProject} onClick={() => deleteProjectById(p.id, user.email)}>
+                          Delete
+                          <DeleteIcon className={classes.optionsIcon} />
+                        </div>
+                      </Grid>
                     </Grid>
-                  </Grid>
 
 
-                  <Grid item xs={1}></Grid>
-                </Fragment>
+                    <Grid item xs={1}></Grid>
+                  </Fragment>
 
-              )
-            })}
+                )
+              })}
+            </Grid>
           </Grid>
-        </Grid>
-      </PageContainer>
-    </Fragment>
-  )
-}
+        </PageContainer>
+      </Fragment>
+    )
+  }
 
-const container = compose<Props, {}>(
-  withRouter,
-  connect<StateProps, DispatchProps, {}, RootState>(
-    (state: RootState) => ({
-      loading: state.domains.shapediver.loading,
-      projects: state.domains.shapediver.projects
-    }),
-    {
-      loadProjectsByUsername,
-      deleteProjectById
-    }
-  )
-)(ListProjects);
+  const container = compose<Props, {}>(
+    withRouter,
+    connect<StateProps, DispatchProps, {}, RootState>(
+      (state: RootState) => ({
+        loading: state.domains.shapediver.loading,
+        projects: state.domains.shapediver.projects
+      }),
+      {
+        loadProjectsByUsername,
+        deleteProjectById
+      }
+    )
+  )(ListProjects);
 
-export default container;
+  export default container;
 
 
 
