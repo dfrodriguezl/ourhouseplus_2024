81289920150ce1bdd126caa5b226da47cd09c676
Diego <dfrodriguezl94@gmail.com>
2024-02-17 18:42:08 -0500

    First folder muiv5

--- src/domains/common/components/Slogan.tsx
+++ src/domains/common/components/Slogan.tsx
@@ -1,4 +1,7 @@
-import { Box, createStyles, makeStyles, Theme } from '@material-ui/core'
+import { Box, Theme } from '@mui/material';
+
+import createStyles from '@mui/styles/createStyles';
+import makeStyles from '@mui/styles/makeStyles';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -6,10 +9,7 @@ const useStyles = makeStyles((theme: Theme) =>
       textAlign: 'center',
       color: 'black',
       position: 'relative',
-      letterSpacing: '1px',
-      [theme.breakpoints.down('sm')]: {
-        marginBottom: '10vh'
-      },
+      letterSpacing: '1px'
     },
     header: {
       paddingBottom: 5,
