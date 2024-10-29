267414952768ed7dd0dcfee5ab5be505535c273c
Diego Rodriguez <dfrodriguezl94@gmail.com>
2024-10-29 16:11:08 -0500

    Flow delete project completed

---
+++ src/domains/core/components/ModalDeleteProject.tsx
@@ -0,0 +1,91 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import { Modal, Box, Typography, IconButton, Button, Snackbar } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { Project } from '../models';
+import { deletes, get } from 'app/api';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "70%",
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const styleButton = {
+    color: 'black',
+    borderColor: 'black',
+    marginTop: 20
+}
+
+
+interface OwnProps {
+    open: boolean;
+    onClose: any;
+    project: Project | undefined;
+    getProjects?: any;
+}
+
+type Props = OwnProps;
+const ModalDeleteProject = (props: Props) => {
+    const { open, onClose, project, getProjects } = props;
+    const [projects, setProjects] = useState<Project[]>([]);
+    const { user } = useAuth0();
+    const [openSnackbar, setOpenSnackbar] = useState<boolean>(false);
+
+
+    useEffect(() => {
+        const getProjects = () => {
+            get("/projects/user/" + user?.email).then((data: any) => {
+                setProjects(data.data.Items);
+            })
+        }
+
+        getProjects()
+    }, [user])
+
+    const deleteProject = () => {
+        deletes(`/projects/${project?.idProject}`).then((response) => {
+            getProjects()
+            onClose()
+        })
+    }
+
+    return (
+        <Fragment>
+            <Snackbar
+                open={openSnackbar}
+                autoHideDuration={6000}
+                message="Project removed"
+            />
+            <Modal
+                open={open}
+                onClose={onClose}
+                aria-labelledby="modal-carousel-title"
+                aria-describedby="modal-carousel-description"
+            >
+                <Box sx={style}>
+                    <Box display="flex" justifyContent="space-between" alignItems="center">
+                        <Typography id="modal-carousel-title" variant="h6" component="h2">
+                            Are you sure? Project will be removed definitely from your account
+                            <br />
+                            <strong>Project: {project?.name}</strong>
+                        </Typography>
+                        <IconButton onClick={onClose}>
+                            <CloseIcon />
+                        </IconButton>
+                    </Box>
+                    <Button style={styleButton} variant="outlined" onClick={() => deleteProject()}>Remove project</Button>
+                </Box>
+            </Modal>
+        </Fragment>
+
+    );
+}
+
+export default ModalDeleteProject;
--- src/domains/core/containers/ProjectsMobile.tsx
+++ src/domains/core/containers/ProjectsMobile.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { PageContainer } from '.';
-import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Grid, Theme, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Grid, IconButton, Theme, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { ItemCatalogue, Project } from '../models';
 import { get } from 'app/api';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -10,6 +10,8 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { makeStyles } from '@mui/styles';
 import { useNavigate } from 'react-router-dom';
 import CardFavorite from '../components/CardFavorite';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import ModalDeleteProject from '../components/ModalDeleteProject';
 
 const useStyles = makeStyles((theme: Theme) => ({
     buttonNewProject: {
@@ -34,53 +36,63 @@ const ProjectsMobile = () => {
     const smallScreen = useMediaQuery(theme.breakpoints.down("sm"));
     const classes = useStyles();
     const history = useNavigate();
+    const [openModalDelete, setOpenModalDelete] = useState(false);
+    const handleOpenModal = () => setOpenModalDelete(true);
+    const handleCloseModal = () => setOpenModalDelete(false);
+    const [projectToDelete, setProjectToDelete] = useState<Project|undefined>();
 
     useEffect(() => {
-
-        const getProjects = () => {
-            
-
-            get("/projects/user/" + user?.email).then((data: any) => {
-                
-                get("/items-catalogue").then((item: any) => {
-                    
-                    const projectsWithFurnitures = data.data.Items.map((element: any) => {
-                        let furnituresFiltered: any = [];
-                        if (element.furnitures) {
-                            element.furnitures.map((furniture: any) => {
-                                const itemFiltered = item.data.Items.filter((o: any) => String(o.idItem) === String(furniture));
-                                furnituresFiltered.push(itemFiltered[0]);
-                            }, [])
-                        }
-
-                        element.furnituresFiltered = furnituresFiltered;
-
-                        return element;
-
-                    }, [])
-
-                    console.log("FURNITURES FILTERED", projectsWithFurnitures);
-                    setProjects(projectsWithFurnitures);
-                })
-
-
-
-                
-            })
-        }
-
         getProjects()
     }, [user])
 
+    const getProjects = () => {
+
+
+        get("/projects/user/" + user?.email).then((data: any) => {
+
+            get("/items-catalogue").then((item: any) => {
+
+                const projectsWithFurnitures = data.data.Items.map((element: any) => {
+                    let furnituresFiltered: any = [];
+                    if (element.furnitures) {
+                        element.furnitures.map((furniture: any) => {
+                            const itemFiltered = item.data.Items.filter((o: any) => String(o.idItem) === String(furniture));
+                            furnituresFiltered.push(itemFiltered[0]);
+                            return itemFiltered[0];
+                        }, [])
+                    }
+
+                    element.furnituresFiltered = furnituresFiltered;
+
+                    return element;
+
+                }, [])
+
+                setProjects(projectsWithFurnitures);
+            })
+
+
+
+
+        })
+    }
+
     const goToNewProject = () => {
         history("/newProject")
+    }
+
+    const deleteProject = (project: Project) => {
+        setProjectToDelete(project);
+        handleOpenModal()
     }
 
     return (
         <PageContainer background="create-project">
             {isAuthenticated ?
                 smallScreen ?
+                
                     <Grid item container direction="column" alignContent="center" textAlign="center">
+                        <ModalDeleteProject open={openModalDelete} onClose={handleCloseModal} project={projectToDelete} getProjects={getProjects}/>
                         <Typography>PROJECT</Typography>
                         <Box>
                             <FolderOpenIcon fontSize='large' />
@@ -100,6 +112,9 @@ const ProjectsMobile = () => {
                                         <Accordion key={index}>
                                             <AccordionSummary
                                                 expandIcon={<ExpandMoreIcon />}>
+                                                <IconButton aria-label="delete-item" onClick={() => deleteProject(project)}>
+                                                    <RemoveCircleIcon />
+                                                </IconButton>
                                                 <Grid container direction="row" justifyContent="space-around">
                                                     <Typography>
                                                         {project.name}
@@ -116,7 +131,7 @@ const ProjectsMobile = () => {
                                                     <AccordionDetails>
                                                         {project.furnituresFiltered?.map((item: ItemCatalogue, index: number) => {
                                                             return (
-                                                                <CardFavorite key={index} item={item}/>
+                                                                <CardFavorite key={index} item={item} />
                                                             )
                                                         })}
                                                     </AccordionDetails>
